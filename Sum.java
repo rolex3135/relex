fd75182e2ebe20e7e94fb7b3bee8c61c5025b494
surya <suryagede987@gmail.com>
2024-05-08 12:01:06 +0530

    first sum

--- Sum.java
+++ Sum.java
@@ -1,8 +1,8 @@
 public class Sum {
     public static void main(String[] args) {
-        int a=22;
-        int b=43;
-        int sum =a+b;
+        int a=3;
+        int b=9;
+        int sum=a+b;
         System.out.println(sum);
     }
 }
